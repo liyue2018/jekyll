bee17fb9ffdded461f5e4cb9c845603be4f6cac9
liyue2018 <1059869380@qq.com>
2018-05-11 18:08:00 +0800

    add jquery effect

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 /*
-* @Author: yongze-chen
+* @Author: liyue
 * @Date:   2018-04-08 15:37:45
-* @Last Modified by:   yongze-chen
-* @Last Modified time: 2018-04-08 15:38:22
+* @Last Modified by:   liyue2018
+* @Last Modified time: 2018-04-13 12:45:47
 */
 var init = require('./build/gulpfile.init.js');
 var prod = require('./build/gulpfile.prod.js');
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/*
+* @Author: liyue2018
+* @Date:   2018-04-08 16:20:05
+* @Last Modified by:   liyue2018
+* @Last Modified time: 2018-05-09 13:52:21
+*/
+
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './node_modules/bootstrap/dist/js/bootstrap.min.js';
