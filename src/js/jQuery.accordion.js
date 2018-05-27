294c940916f0b81024c4f5b534c888e5f3e18d3d
liyue2018 <1059869380@qq.com>
2018-05-28 01:28:40 +0800

    add table

---
+++ src/js/jQuery.accordion.js
@@ -0,0 +1,35 @@
+/*
+* @Author: liyue2018
+* @Date:   2018-05-22 23:38:15
+* @Last Modified by:   liyue2018
+* @Last Modified time: 2018-05-25 16:10:01
+*/
+$(function(){
+  // 手风琴效果
+  
+  $.fn.accordion = function(colors,width){
+    colors = colors || [];
+    width = width || 0;
+    var $li = this.find('li.item');
+    console.log($li.length - 1)
+    var boxLength = this.width();
+    var maxLength = boxLength - ($li.length) * width;
+    var avgLength = boxLength / ($li.length + 1);
+
+    //更改li的背景色
+    $li.each(function(i,e){
+        $(e).css('backgroundColor',colors[i]);
+    });
+
+    //给所有的li注册鼠标经过事件
+    $li.on('mouseenter',function(){
+        $(this).stop().animate({width:maxLength}).siblings().stop().animate({width: width});
+    });
+
+    //给所有的li注册鼠标离开事件
+    $li.on('mouseleave',function(){
+        $li.stop().animate({width:avgLength});
+    });
+    return this;
+  }
+})
